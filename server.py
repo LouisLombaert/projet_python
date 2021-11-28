c0edecebc2667810a841e68d51f957332746410e
NathanGregoar <64077468+NathanGregoar@users.noreply.github.com>
2021-11-28 17:07:28 +0100

    Tests Unitaires

--- server.py
+++ server.py
@@ -1,21 +1,35 @@
 import socket
-
-myIP="127.0.0.1"                                        #IP local ou IP server si distant
-myPort=64000
-bufferSize=1024
+import unittest
 
 
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)     #Créé un objet socket
-s.bind((myIP, myPort))                                  #Initialisation socket
-s.listen(1)                                             #Ecoute une connexion
-(cl, adress) = s.accept()                               #Récupere coordonée client et attibution variable
+def function_server():
+    """création d'une conversation entre le client et le serveur"""
+    myIP="127.0.0.1"                                        #IP local ou IP server si distant
+    myPort=63000
+    buffer_size = 1024
 
-rep=''
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Créé un objet socket
+    s.bind((myIP, myPort))  # Initialisation socket
+    s.listen(1)  # Ecoute une connexion
+    (cl, adress) = s.accept()  # Récupere coordonée client et attibution variable
 
-while(rep != 'Bye'):
-    msg=cl.recv(bufferSize)                             #Récupere le message encodé
-    msg=msg.decode()
-    print("                                             Nathan 1 : '{}'".format(msg))
+    rep = ''
 
-    rep=input('Votre réponse : ')
-    cl.send(rep.encode())
+    while rep != 'Bye':
+        """tant que la réponse du client n'est pas 'Bye', continuer la conversation"""
+        msg = cl.recv(buffer_size)  # Récupere le message encodé
+        msg = msg.decode()
+        print("                                             Nathan 1 : '{}'".format(msg))
+
+        rep = input('Votre réponse : ')
+        cl.send(rep.encode())
+
+class TestServer(unittest.TestCase):
+    def test_str(self):
+        rep = ''
+        self.assertEqual(rep, '')
+
+if __name__ == "__main__":
+    """appel de la fonction function_server"""
+    #unittest.main()
+    function_server()
