b8d2b1944ca0c992fd4fae731af9f75d1ff2ff2f
creativezigzz <64020049+creativezigzz@users.noreply.github.com>
2021-12-10 02:36:38 +0100

    Rangement dans les dossiers en respectant la pep8
    Signed-off-by: creativezigzz <64020049+creativezigzz@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-
+from models.category import Categories
+from models.channel import Channel
+from models.message import Message
+from models.user import User
+from models.mongo_connect import MongoConnector
 
 
 if __name__ == "__main__":
